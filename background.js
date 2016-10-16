106b3715f88d03552d4399874764635a7b4542ee
Maldo <maldo87@gmail.com>
2016-10-16 13:26:55 +0200

    Update url times

--- background.js
+++ background.js
@@ -8,9 +8,16 @@ chrome.webNavigation.onCompleted.addListener(function(details) {
         chrome.tabs.get(details.tabId, function(tab) {
             if(tab.url === details.url) {
 
-            	if(urls.hasOwnProperty[tab.url]) {
-            		urls[tab.url] = 0;
-            	}
+            	if(!urls.hasOwnProperty[tab.url]) {
+            		urls[tab.url] = {
+            			time: 0,
+            			current: true,
+            			delta: details.timeStamp
+            		};
+        		} else {
+        			urls[tab.url].current = true;
+        		}
+            	updateTime(tab.url, details.timeStamp);
 
 
 
@@ -23,7 +30,7 @@ chrome.webNavigation.onCompleted.addListener(function(details) {
 			    		"tab_id": details.tabId,
 			    		"url": tab.url,
 			    		"visit_start": details.timeStamp,
-			    		"duration": urls[tab.url],
+			    		"duration": urls[tab.url].time,
 			    		"referrer": res[0]
 			    	});
 			    	console.log(to_send)
@@ -32,3 +39,31 @@ chrome.webNavigation.onCompleted.addListener(function(details) {
         });
     }
 });
+
+
+function updateTime(urlActive, timeStamp) {
+	for(let url in urls) {
+		data = urls[url];
+		if(data.current && urlActive != url) {
+			data.current = false;
+			data.time += timeStamp - data.delta;
+			urls[url] = data;
+		}
+	}
+	console.log(urls)
+}
+
+/*function getCurrentActiveTab() {
+    chrome.tabs.query({
+        currentWindow: true,
+        active: true
+    }, function(tabs) {
+    	let url + tabs[0].url;
+        getURL(tabs[0].url);
+        clearInterval(interval);
+        interval = null;
+        interval = setInterval(function() {
+            ();
+        }, updateTime);
+    });
+};*/
